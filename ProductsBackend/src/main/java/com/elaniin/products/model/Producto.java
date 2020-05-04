6789a475c74e52756b6f6f354f9b7eb281a9973b
Rodrigo Valle <rodrigoahv@hotmail.com>
2020-05-03 20:50:21 -0600

    Validaciones de datos
    Validaciones de tipos de datos, longitudes, formatos y requeridos

--- ProductsBackend/src/main/java/com/elaniin/products/model/Producto.java
+++ ProductsBackend/src/main/java/com/elaniin/products/model/Producto.java
@@ -9,6 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "producto")
@@ -20,21 +24,30 @@ public class Producto {
 	@Column(name = "id_producto", updatable = false, nullable = false)
 	private Integer idProducto;
 
+	@Size(max = 255, message = "SKU no debe tener más de 255 caracteres")
 	@Column(name = "sku", nullable = true)
 	private String sku;
 	
+	@Size(max = 255, message = "Nombre no debe tener más de 255 caracteres")
+	@NotBlank(message = "Nombre es un valor requerido")
 	@Column(name = "nombre", nullable = false)
 	private String nombre;
 
+	@Digits(message = "Cantidad debe ser un valor numérico entero", fraction = 0, integer = 10)
+	@NotNull(message = "Cantidad es un valor requerido")
 	@Column(name = "cantidad", nullable = false)
 	private Integer cantidad;
 
+	@Digits(message = "Precio debe ser un valor numérico con dos decimales", fraction = 2, integer = 19)
+	@NotNull(message = "Precio es un valor requerido")
 	@Column(name = "precio", nullable = false)
 	private BigDecimal precio;
 	
+	@Size(max = 255, message = "Descripción no debe tener más de 255 caracteres")
 	@Column(name = "descripcion", nullable = true)
 	private String descripcion;
 	
+	@Size(max = 255, message = "Imagen no debe tener más de 255 caracteres")
 	@Column(name = "imagen", nullable = true)
 	private String imagen;
 
--- ProductsBackend/src/main/java/com/elaniin/products/model/Usuario.java
+++ ProductsBackend/src/main/java/com/elaniin/products/model/Usuario.java
@@ -9,6 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "usuario")
@@ -20,21 +25,33 @@ public class Usuario {
 	@Column(name = "id_usuario", updatable = false, nullable = false)
 	private Integer idUsuario;
 
+	@Size(max = 255, message = "Nombre no debe tener más de 255 caracteres")
+	@NotBlank(message = "Nombre es un valor requerido")
 	@Column(name = "nombre", nullable = false)
 	private String nombre;
 	
+	@Digits(message = "Teléfono debe ser un valor numérico", fraction = 0, integer = 255)
+	@Size(max = 255, message = "Teléfono no debe tener más de 255 dígitos")
 	@Column(name = "telefono", nullable = true)
 	private String telefono;
-
+	
+	@Size(max = 255, message = "Username no debe tener más de 255 caracteres")
+	@NotBlank(message = "Username es un valor requerido")
 	@Column(name = "username", nullable = false, unique = true)
 	private String username;
 	
+	@Past(message = "La fecha de nacimiento debe ser anterior a la fecha actual")
 	@Column(name = "fecha_nacimiento", nullable = true)
 	private LocalDateTime fechaNacimiento;
 
+	@Email(message = "El email ingresado no es válido")
+	@Size(max = 255, message = "Email no debe tener más de 255 caracteres")
+	@NotBlank(message = "Email es un valor requerido")
 	@Column(name = "email", nullable = false, unique = true)
 	private String email;
 	
+	@Size(max = 100, message = "Password no debe tener más de 100 caracteres")
+	@NotBlank(message = "Password es un valor requerido")
 	@Column(name = "password", nullable = false)
 	private String password;
 
